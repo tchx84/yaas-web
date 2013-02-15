889ecce98442f0b678f6de70b13dfad3696a17f1
Daniel Drake <dsd@laptop.org>
2013-02-15 15:45:11 -0600

    Update to Rails 3.2
    These are the changes introduced by: rake rails:update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-YaasWeb::Application.config.session_store :cookie_store, :key => '_yaas-web_session'
+YaasWeb::Application.config.session_store :cookie_store, key: '_yaas_web_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
